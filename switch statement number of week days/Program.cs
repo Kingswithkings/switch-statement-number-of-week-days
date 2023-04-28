62ffe0095476d994a34e24733b261684563dc27c
kingswithkings <kingsuthanaidogu@gmail.com>
2023-04-28 17:21:30 +0100

    Add project files.

---
+++ switch statement number of week days/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace switch_statement_number_of_week_days
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayofweek = 7;
+            switch (dayofweek)
+            {
+                case 0:
+                    Console.WriteLine("sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("friday");
+                    break;
+                case 6:
+                    Console.WriteLine("saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("sunday");
+                    break;
+            }
+        }
+    }
+}
